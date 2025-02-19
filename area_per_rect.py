d9351009e70d4848405b6bc9373bc8b203dbb1e5
Dylan Mutabazi <dylan.mutabazi@stu.ocsb.ca>
2025-02-19 23:18:01 +0000

    Added

---
+++ area_per_rect.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Created by Dylan Mutabazi
+# Date :2025-02-19
+# This program calculates the perimeter & area of a rectangle
+
+
+def main():
+    print("If a rectangle has dimensions:")
+    print("9cm for the length and 6cm for the width")
+    print("")
+    print("Area is {}cm^2".format(9 * 6))
+    print("Perimeter is {}cm".format(2 * (9 + 6)))
+
+
+if __name__ == "__main__":
+    main()
